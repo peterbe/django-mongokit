4f92b2fc7c22c81fdc568cfd99ac68e1b4404650
Peter Bengtsson <peter@fry-it.com>
2010-03-10 11:00:08 +0000

    removing dependency on pkg_resources

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # Copyright (c) 2010 Peter Bengtsson, peter@fry-it.com
 from distutils.core import setup
-from pkg_resources import resource_string
-
-
 
 setup(name='django-mongokit',
-      version=resource_string(__name__, 'django_mongokit/version.txt'),
+      version=open('django_mongokit/version.txt').read(),
       author="Peter Bengtsson",
       author_email="peter@fry-it.com",
       url="http://github.com/peterbe/django-mongokit",
       description='Bridging Django to MongoDB with the MongoKit ODM',
-      long_description=resource_string(__name__, 'README.md'),
+      long_description=open('README.md').read(),
       package_dir={'djangomongokitlib':'django-mongokitlib'},
       packages=['django_mongokit', 'django_mongokit.mongodb'],
       package_data={'django_mongokit':['version.txt']},
-      classifiers=['Development Status :: 3 - Alpha', # remember to change this one day to '5 - Production/Stable'
+      classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Web Environment',
                    'Framework :: Django',
                    'Intended Audience :: Developers',
